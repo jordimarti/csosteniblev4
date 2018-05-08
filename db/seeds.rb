a6b4b2050f7dd8a778c68173e18a3a12864eca5a
Jordi Martí Muñoz <jordimarti@me.com>
2018-05-09 00:44:49 +0200

    Afegeix categories i ecoetiquetes als seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,23 @@
 
 Categoria.create(nom_ca: "Mitjans i equips auxiliars", nom_es: "Medios y equipos auxiliares", nom_en: "Means and auxiliary equipment")
 Categoria.create(nom_ca: "Enderrocs i gestió de residus", nom_es: "Demoliciones y gestión de residuos", nom_en: "Demolitions and waste management")
+Categoria.create(nom_ca: "Terreny, contenció i fonamentació", nom_es: "Terreno, contención y cimentación")
+Categoria.create(nom_ca: "Estructures", nom_es: "Estructuras")
+Categoria.create(nom_ca: "Cobertes", nom_es: "Cobertes")
+Categoria.create(nom_ca: "Tancaments i divisòries", nom_es: "Cerramientos y divisorias")
+Categoria.create(nom_ca: "Aïllaments", nom_es: "Aislamientos")
+Categoria.create(nom_ca: "Impermeabilitzacions i tractaments d'humitats", nom_es: "Impermeabilizaciones y tratamientos de humedades")
+Categoria.create(nom_ca: "Revestiments", nom_es: "Revestimientos")
+Categoria.create(nom_ca: "Instal·lacions", nom_es: "Instalaciones")
+Categoria.create(nom_ca: "Urbanització", nom_es: "Urbanización")
 Ecoetiqueta.create(nom_ca: "Distintiu de Garantia de Qualitat Ambiental", nom_es: "Distintiu de Garantia de Qualitat Ambiental", nom_en: "Distintiu de Garantia de Qualitat Ambiental")
 Ecoetiqueta.create(nom_ca: "Ecolabel", nom_es: "Ecolabel", nom_en: "Ecolabel")
 Ecoetiqueta.create(nom_ca: "Aenor Medio Ambiente", nom_es: "Aenor Medio Ambiente", nom_en: "Aenor Medio Ambiente")
+Ecoetiqueta.create(nom_ca: "Der Blaue Engel", nom_es: "Der Blaue Engel", nom_en: "Der Blaue Engel")
+Ecoetiqueta.create(nom_ca: "The Swan", nom_es: "The Swan", nom_en: "The Swan")
+Ecoetiqueta.create(nom_ca: "NF Environment", nom_es: "NF Environment", nom_en: "NF Environment")
+Ecoetiqueta.create(nom_ca: "DAPconstrucción", nom_es: "DAPconstrucción", nom_en: "DAPconstrucción")
+Ecoetiqueta.create(nom_ca: "DAP Alemania", nom_es: "DAP Alemania", nom_en: "DAP Alemania")
+Ecoetiqueta.create(nom_ca: "DAP Suècia", nom_es: "DAP Suècia", nom_en: "DAP Suècia")
+
+
