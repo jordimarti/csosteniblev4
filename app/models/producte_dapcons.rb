e3cbf6ead194dc774c3ca5274e7b62f79cc656d3
Jordi Martí Muñoz <jordimarti@me.com>
2018-10-07 20:43:47 +0200

    Canvis en el llistat de productes DAPcons, incloent foto

--- app/models/producte_dapcons.rb
+++ app/models/producte_dapcons.rb
@@ -3,5 +3,6 @@ class ProducteDapcons < ApplicationRecord
 	has_many :document_dapcons
 	has_many :indicadors, dependent: :destroy, inverse_of: :producte_dapcons
 	accepts_nested_attributes_for :indicadors, allow_destroy: true, reject_if: ->(attrs) { attrs['tipus_indicador_id'].blank? }
+	has_one_attached :imatge_producte_dapcons
 
 end
