c380f8771550cbccf73d974c07740f62a8d3ab79
Jordi Martí Muñoz <jordimarti@me.com>
2019-04-21 19:13:09 +0200

    Devise torna a la pàgina d'origen després de fer login, no a la home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_user_location!, if: :storable_location?
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
@@ -12,7 +13,11 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(user)
-    home_index_path
+    stored_location_for(user) || super
+  end
+
+  def after_sign_out_path_for(user)
+    marketcons_index_path
   end 
 
   protected
@@ -21,4 +26,14 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
+
+  private
+  def storable_location?
+      request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+    end
+
+    def store_user_location!
+      store_location_for(:user, request.fullpath)
+    end
+
 end
