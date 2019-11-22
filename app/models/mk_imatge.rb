56f169eade8f5403708a51849680824ff745b4f6
Jordi Martí Muñoz <jordimartimunoz@iMac-de-Jordi.local>
2019-11-22 13:41:45 +0100

    Millora les imatges

--- app/models/mk_imatge.rb
+++ app/models/mk_imatge.rb
@@ -3,4 +3,17 @@ class MkImatge < ApplicationRecord
 	belongs_to :mk_product
 	has_one_attached :mk_product_imatge
 
+
+	def miniatura_imatge
+		return self.mk_product_imatge.variant(resize: '200x200', auto_orient: true).processed
+	end
+
+	def petita_imatge
+		return self.mk_product_imatge.variant(resize: '300x300', auto_orient: true).processed
+	end
+
+	def standard_imatge
+		return self.mk_product_imatge.variant(resize: '800x800', auto_orient: true).processed
+	end
+
 end
